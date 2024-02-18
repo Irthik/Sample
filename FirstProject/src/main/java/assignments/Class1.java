82de447f077a8d596c8811e895e594ae5795ec12
1 <1@DESKTOP-DKUGJQB>
2024-02-18 10:51:22 +0530

    Assignment

---
+++ FirstProject/src/main/java/assignments/Class1.java
@@ -0,0 +1,17 @@
+package assignments;
+//import java.util.Scanner;
+
+public class Class1 {
+	double bp=1000;
+	double ded=200;
+	double bonus=100;
+	
+	public void pay()
+	{
+		//Scanner a=new Scanner(System.in);
+		double bp=this.bp;
+		double ded=this.ded;
+		double bonus=this.bonus;
+	}
+
+}
---
+++ FirstProject/src/main/java/assignments/Class2.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class Class2 extends Class1 {
+	double hra;
+	double pf;
+	public void calculate()
+	{
+		super.pay();
+		this.hra=bp*0.05;
+		this.pf=bp*0.2;
+		
+	}
+
+}
